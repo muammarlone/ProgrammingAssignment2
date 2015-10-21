86124e72e6d89d3027ffe5d4546d104a961cf2ce
muammarlone <muammarlone@gmail.com>
2015-10-21 17:49:51 -0500

    Modified functions
    Signed-off-by: muammarlone <muammarlone@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Framework to create square matrix / getters & setters functions
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) inverse <<- inv
+        getinv <- function() inverse
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
+## Check if object is already in memory use it or else use solve to calculate the inverse of the matrix
+## From Matrix Algebra the definition of Inverse of a Matrix is A * A^-1 = I (invoulatory Matrix). 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinv()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinv(inverse)
+        invserse
 }
